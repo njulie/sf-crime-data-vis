cafbdbcc37c35f856778313c42e68a1fc2260acb
haiyinwang <haiyinwangh@gmail.com>
2016-04-29 14:16:02 -0700

    Successful plotting of all points

--- js/crimeData.js
+++ js/crimeData.js
@@ -1,11 +1,24 @@
 // load the data
-d3.json("../scpd-incidents.json", function(error, crimes) {
+d3.json("scpd-incidents.json", function(error, crimes) {
 	if (error) throw error;
-	console.log(crimes.data[0]);
-
-
-
-
-
-	
+	plotInitialData(crimes.data);
 });
+
+
+function plotInitialData(crimes) {
+	var svgContainer = d3.select("svg");
+
+	console.log(crimes[0]);
+	console.log(projection(crimes[0].Location));
+
+	var circles = svgContainer.selectAll("circle")
+						.data(crimes)
+						.enter()
+						.append("circle");
+
+	var circleAttributes = circles
+							.attr("cx", function (d) { return projection(d.Location)[0]; })
+							.attr("cy", function (d) { return projection(d.Location)[1]; })
+							.attr("r", 5)
+							.style("fill", "purple");
+}
