a98f03e944c67efca1c340dcee45a1fcebd05a4b
haiyinwang <haiyinwangh@gmail.com>
2016-04-30 01:18:25 -0700

    Plotted all crimes and colored by time of day

--- js/crimeData.js
+++ js/crimeData.js
@@ -8,17 +8,31 @@ d3.json("scpd-incidents.json", function(error, crimes) {
 function plotInitialData(crimes) {
 	var svgContainer = d3.select("svg");
 
-	console.log(crimes[0]);
-	console.log(projection(crimes[0].Location));
-
+	// Default Display all crimes
 	var circles = svgContainer.selectAll("circle")
 						.data(crimes)
 						.enter()
 						.append("circle");
 
+	// Color by time of day
+	var color = d3.scale.ordinal()
+				.range(["#ff7f0e", "#17becf", "#1f77b4"]);
+
+
 	var circleAttributes = circles
 							.attr("cx", function (d) { return projection(d.Location)[0]; })
 							.attr("cy", function (d) { return projection(d.Location)[1]; })
-							.attr("r", 5)
-							.style("fill", "purple");
+							.attr("r", 2)
+							// Color By Time of Day
+							.style("fill", function(d) {
+								//Get hour
+								var hour = parseInt(d.Time.split(":")[0]);
+								if(hour > 4 && hour <= 12) {
+									return color("morning");
+								} else if (hour > 12 && hour <= 20) {
+									return color("afternoon");
+								} else {
+									return color("evening");
+								}
+							});
 }
