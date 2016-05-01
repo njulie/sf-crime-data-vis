84a9756be6acdb31ebe7baa5dbb54b28b4f0e888
julie <eiluj.ni@gmail.com>
2016-05-01 02:15:55 -0700

    cleaned up the structure of enter update exit

--- js/crimeData.js
+++ js/crimeData.js
@@ -2,57 +2,58 @@
 // load the data
 d3.json("scpd-incidents.json", function(error, crimes) {
 	if (error) throw error;
-	updateData(crimes.data);
+
+	update(crimes.data);
 	setUpControls(crimes.data);
 });
 
 // Initial Visualization of the Crime Data
-function updateData(crimes) {
+function update(crimes) {
 	var svgContainer = d3.select("svg");
+
 
 	// Default Display all crimes
 	var circles = svgContainer.selectAll("circle")
-						.data(crimes);
+						.data(crimes)
+						.attr("class", "update");
 
-	circles.attr("class", "update");
-
-	circles.enter().append("circle").attr("class","enter");
 
 	// Color by time of day
-	var color = d3.scale.ordinal()
-				.range(["#ff7f0e", "#17becf", "#1f77b4"]);
+	var color = d3.scale.ordinal().range(["#ff7f0e", "#17becf", "#1f77b4"]);
 
 
-	// Set Color Attributes
-	var circleAttributes = circles
-							.attr("cx", function (d) { return projection(d.Location)[0]; })
-							.attr("cy", function (d) { return projection(d.Location)[1]; })
-							.attr("r", 2)
-							// Color By Time of Day
-							.style("fill", function(d) {
-								//Get hour
-								var hour = parseInt(d.Time.split(":")[0]);
-								if(hour > 4 && hour <= 12) {
-									return color("morning");
-								} else if (hour > 12 && hour <= 20) {
-									return color("afternoon");
-								} else {
-									return color("evening");
-								}
-							});
+	circles.enter().append("circle").attr("class","enter")
+		.attr("cx", function (d) { return projection(d.Location)[0]; })
+		.attr("cy", function (d) { return projection(d.Location)[1]; })
+		.attr("r", 2)
+
+		// Set Color Attributes
+		.style("fill", function(d) {
+			//Get hour
+			var hour = parseInt(d.Time.split(":")[0]);
+			if(hour > 4 && hour <= 12) {
+				return color("morning");
+			} else if (hour > 12 && hour <= 20) {
+				return color("afternoon");
+			} else {
+				return color("evening");
+			}
+		});
+	
 
 	circles.exit().remove();
-}
+};
+
 
 function setUpControls(crimes) {
 
 	// Shallow Copy of Data to display
-	var curr_crimes = crimes.slice(0);
+	var curr_crimes = crimes;
 
 	// Handle Weekday Checkbox Settings
 	$(".weekdayCheckbox").on("click", function(){
 		var day = this.value;
-		if(this.checked) {
+		if (this.checked) {
 			var temp_vals = crimes.filter(function(value) {
 				return value.DayOfWeek === day;
 			});
@@ -62,9 +63,7 @@ function setUpControls(crimes) {
 				return value.DayOfWeek !== day;
 			});
 		}
-		updateData(curr_crimes);
-	});
+		update(curr_crimes);
+	});	
 
-	
-
-}
+};
